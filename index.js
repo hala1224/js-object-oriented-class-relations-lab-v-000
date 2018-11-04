2cd68aedb8c1ea72fddf95a406ec6b6645d0faec
Natalie Halawani <hala1224@gmail.com>
2018-11-04 19:34:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,45 @@
+let driverId=0;
+let store={drivers: [], passengers: [], trips: []};
+
+class Driver {
+  constructor(name){
+    this.id=++driverId;
+    this.name=name;
+    store.drivers.push(this);
+  }
+  //  returns all of the trips that driver had taken
+  
+  trips(){
+    // driverId= driverId - 1;
+    return store.trips.filter(
+      function(trip) {
+        return trip.driverId === this.id;
+      }.bind(this)
+    );
+  }
+  
+  
+}
+
+let passengerId=0;
+
+class Passenger {
+  constructor(name){
+    this.id=++passengerId;
+    this.name=name;
+    store.passengers.push(this);
+  }
+}
+
+let tripId=0;
+
+class Trip {
+  constructor(driver, passenger){
+    this.id=++tripId;
+    let myDriver= new Driver(driver);
+    this.driverId= myDriver.id;
+    let myPassenger = new Passenger(passenger);
+    this.passengerId= myPassenger.id;
+    store.trips.push(this);
+  }
+}
