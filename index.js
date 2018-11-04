eec09682aaad2c440ed857888abe6a141b83bc9a
Natalie Halawani <hala1224@gmail.com>
2018-11-04 20:35:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,6 +36,19 @@ class Passenger {
     this.name=name;
     store.passengers.push(this);
   }
+  
+   trips(){
+      return store.trips.filter(trip =>{
+      return trip.passengerId == this.id;
+    });
+  }
+
+   drivers() {
+      return this.trips().map(trip => {
+      return trip.driver();
+      });
+   }
+  
 }
 
 let tripId=0;
